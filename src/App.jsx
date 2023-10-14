cb126c31bcc3462af22546553343a952bb791f2b
Rushikesh Bunge <68096217+rushikesh1799@users.noreply.github.com>
2023-10-14 16:19:22 +0530

    Update: Dispatched fetch expense actions

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import { Route, Routes } from "react-router";
 import "./App.css";
 import Dashboard from "./Components/Dashboard";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { fetchExpense } from "./Redux/Actions/ExpenseAction";
 
 function App() {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchExpense());
+    }, [dispatch]);
+
     return (
         <>
             <Routes>
